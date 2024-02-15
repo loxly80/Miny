b200a7f2d46aeb08d072b6c66b1c3a0b4b77dc26
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-02-15 15:02:19 +0100

    Přidejte soubory projektu.

---
+++ Miny/Bomb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace Miny
+{
+  public class Bomb
+  {
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public int Timeout { get; private set; }
+
+    private int limitX;
+    private int limitY;
+    private Random random;
+    private Timer timer;
+
+    public Bomb(int limitX, int limitY)
+    {
+      this.limitX = limitX;
+      this.limitY = limitY;
+      random = new Random();
+      X = random.Next(0, limitX);
+      Y = random.Next(0, limitY);
+      Timeout = Math.Max(limitX, limitY) * 1000;
+      timer = new Timer();
+      timer.Interval = Timeout;
+      timer.Elapsed += Timer_Elapsed;
+      timer.Start();
+    }
+
+    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+      X = random.Next(0, limitX);
+      Y = random.Next(0, limitY);
+    }
+  }
+}
---
+++ Miny/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Miny
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Bomb bomb = new Bomb(5, 5);
+
+      while (true)
+      {
+        Console.Clear();
+        Console.WriteLine($"Bomba je na souřadnicích x={bomb.X}; y={bomb.Y}");
+        Thread.Sleep(500);
+      }
+    }
+  }
+}
